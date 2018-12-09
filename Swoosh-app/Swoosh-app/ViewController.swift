127ce628b4e21d5c0a8d52747cc46c9a03b75731
Mateusz Jur <mateusz.jur92@gmail.com>
2018-12-09 02:52:22 +0100

    UI Change

--- Swoosh-app/Swoosh-app/ViewController.swift
+++ Swoosh-app/Swoosh-app/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        swoosh.frame = CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>)
     }
 
 
